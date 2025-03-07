71521bf91a9fababef233436687224b2339f2efc
stdiodh <playlistdh@gmail.com>
2025-03-07 17:44:50 +0900

    fix : test 주석

--- src/test/kotlin/com/example/A/I_Post_Web_Server/ApplicationTests.kt
+++ src/test/kotlin/com/example/A/I_Post_Web_Server/ApplicationTests.kt
@@ -1,13 +1,12 @@
 package com.example.A.I_Post_Web_Server
 
-import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class ApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
+//	@Test
+//	fun contextLoads() {
+//	}
 
 }
