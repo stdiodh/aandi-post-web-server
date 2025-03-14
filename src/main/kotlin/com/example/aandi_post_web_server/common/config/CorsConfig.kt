3a623d8fb6a4be21e407c472f52e9b1f57eace45
stdiodh <playlistdh@gmail.com>
2025-03-14 19:36:22 +0900

    Cors 등록

--- src/main/kotlin/com/example/aandi_post_web_server/common/config/CorsConfig.kt
+++ src/main/kotlin/com/example/aandi_post_web_server/common/config/CorsConfig.kt
@@ -1,18 +1,19 @@
 package com.example.aandi_post_web_server.common.config
 
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.reactive.config.CorsRegistry
 import org.springframework.web.reactive.config.WebFluxConfigurer
 
 @Configuration
 class CorsConfig : WebFluxConfigurer {
-    @Bean
+
     override fun addCorsMappings(registry: CorsRegistry) {
 
-        registry.addMapping("/**")
+        registry.addMapping("/api/**")
             .allowedOriginPatterns("https://aandi-report-web.firebaseapp.com/*")
             .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .allowedHeaders("*")
             .allowCredentials(true)
+        super.addCorsMappings(registry)
     }
 }
